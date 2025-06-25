4e9736efbe16ce35a85a4ea48e80646887b0a19a
Nasake <73486787+Nasake46@users.noreply.github.com>
2025-06-25 10:05:18 +0200

    middleware-proxy

---
+++ src/server.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import cors from 'cors';
+const app = express();
+import { createProxyMiddleware } from 'http-proxy-middleware';
+import 'dotenv/config';
+
+app.use(cors());
+app.use(express.json());
+
+const productsProxy = createProxyMiddleware({
+    target: process.env.PRODUCTS_SERVICE_URL,
+    changeOrigin: true,
+    pathRewrite: {
+        '^/api/products': '/products'
+    },
+    onError: (err, req, res) => {
+        console.error('Proxy error:', err);
+        res.status(500).json({ error: 'Proxy error' });
+    },
+});
+
+app.get('/', (req, res) => { 
+    res.send('Welcome to the Gateway API');
+});
+
+app.get('/api/products', productsProxy);
+
+app.listen(process.env.PORT || 4000, () => {
+    console.log(`Gateway API is running on port ${process.env.PORT || 4000}`);
+});
